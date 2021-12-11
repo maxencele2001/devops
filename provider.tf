0e4c73eeb22fda64aa57aca3d60d05842cd239d8
Maxence CROSSE <maxence@superpictor.com>
2021-12-11 15:55:55 +0100

    fix bug merge

--- provider.tf
+++ provider.tf
@@ -2,7 +2,6 @@
 #
 
 terraform {
-<<<<<<< HEAD
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -16,6 +15,4 @@ provider "aws" {
   region                  = var.aws-region
   shared_credentials_file = "/mnt/c/Users/Maxence CROSSE/IdeaProjects/count/aws/credentials-ynov"
 }
-=======
-}
->>>>>>> 3cb295dae2b11e338cea6ac065c595bba19c111f
+
